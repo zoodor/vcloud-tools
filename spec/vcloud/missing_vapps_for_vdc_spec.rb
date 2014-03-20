60f8090dff0b8dfac6c7e07c4ebc4e9349f8b5fb
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2014-03-20 14:36:06 +0000

    Inline a constant

--- spec/vcloud/missing_vapps_for_vdc_spec.rb
+++ spec/vcloud/missing_vapps_for_vdc_spec.rb
@@ -3,13 +3,11 @@ require 'spec_helper'
 module Vcloud
   describe MissingVappsForVdc do
 
-    VDC_NAME = 'my vdc'
-
     it 'should return a list of vapps only present in the environment' do
       query_runner = double(:query_runner, :run => [{:name => 'that'}, {:name => 'this'}])
       vapps_config = double(:vdc_vapps_config, :vapps => [{:name => 'this'}, {:name => 'the other'}])
 
-      vapp_diff = MissingVappsForVdc.new(VDC_NAME, query_runner, vapps_config)
+      vapp_diff = MissingVappsForVdc.new('my vdc', query_runner, vapps_config)
       vapp_names = vapp_diff.from_config
 
       vapp_names.should == ['that']
@@ -20,7 +18,7 @@ module Vcloud
       vapps_config = double(:vdc_vapps_config, :vapps => [{:name => 'this'}, {:name => 'the other'}])
 
 
-      vapp_diff = MissingVappsForVdc.new(VDC_NAME, query_runner, vapps_config)
+      vapp_diff = MissingVappsForVdc.new('my vdc', query_runner, vapps_config)
       vapp_names = vapp_diff.from_environment
 
       vapp_names.should == ['the other']
@@ -30,7 +28,7 @@ module Vcloud
       query_runner = double(:query_runner, :run => [])
       vapps_config = double(:vdc_vapps_config, :vapps => [{:name => 'this'}, {:name => 'the other'}])
 
-      vapp_diff = MissingVappsForVdc.new(VDC_NAME, query_runner, vapps_config)
+      vapp_diff = MissingVappsForVdc.new('my vdc', query_runner, vapps_config)
       vapp_names = vapp_diff.from_environment
 
       vapp_names.should == ['this', 'the other']
@@ -40,7 +38,7 @@ module Vcloud
       query_runner = double(:query_runner, :run => [{:name => 'that'}, {:name => 'this'}])
       vapps_config = double(:vdc_vapps_config, :vapps => [])
 
-      vapp_diff = MissingVappsForVdc.new(VDC_NAME, query_runner, vapps_config)
+      vapp_diff = MissingVappsForVdc.new('my vdc', query_runner, vapps_config)
       vapp_names = vapp_diff.from_environment
 
       vapp_names.should == []
