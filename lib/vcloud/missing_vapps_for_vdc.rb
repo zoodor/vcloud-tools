b589f4898f8162cb39a0a6b9694b9f6271352299
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2014-03-20 16:15:37 +0000

    Reduce network requests in MissingVappsForVdc
    so that the general usage case doesn't hit the network twice for the
    same data.

--- lib/vcloud/missing_vapps_for_vdc.rb
+++ lib/vcloud/missing_vapps_for_vdc.rb
@@ -5,32 +5,31 @@ module Vcloud
   class MissingVappsForVdc
 
     def initialize(vdc_name, query_runner, vapps_config)
-      @vdc_name     = vdc_name
-      @query_runner = query_runner
-      @vapps_config = vapps_config
+      @env_vapp_names = env_vapp_names(query_runner, vdc_name)
+      @config_vapp_names = config_vapp_names(vapps_config)
     end
 
     def from_config
-      env_vapp_names - config_vapp_names
+      @env_vapp_names - @config_vapp_names
     end
 
     def from_environment
-      config_vapp_names - env_vapp_names
+      @config_vapp_names - @env_vapp_names
     end
 
     private
 
-    def config_vapp_names
-      extract_names(@vapps_config.vapps)
+    def config_vapp_names(vapps_config)
+      extract_names(vapps_config.vapps)
     end
 
-    def env_vapp_names
-      env_vapps = get_vapps_from_env(@vdc_name)
+    def env_vapp_names(query_runner, vdc_name)
+      env_vapps = get_vapps_from_env(query_runner, vdc_name)
       extract_names(env_vapps)
     end
 
-    def get_vapps_from_env(vdc_name)
-      @query_runner.run('vApp', {:filter => "vdcName==#{vdc_name}"})
+    def get_vapps_from_env(query_runner, vdc_name)
+      query_runner.run('vApp', {:filter => "vdcName==#{vdc_name}"})
     end
 
     def extract_names(vapps_list)
