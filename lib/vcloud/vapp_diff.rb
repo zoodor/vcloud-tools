d9b4f74bfd47f7519e4a8a0d1192619139a9d974
Mark <zoodor+github@gmail.com>
2014-03-21 15:03:53 +0000

    Move output generation down into Vcloud::VappDiff

--- lib/vcloud/vapp_diff.rb
+++ lib/vcloud/vapp_diff.rb
@@ -2,13 +2,16 @@ require 'vcloud'
 
 module Vcloud
   class VappDiff
-    def run(vdc_config_dir, vdc_name)
+
+    def run(vdc_config_dir, vdc_name, cli_options = {})
       vapp_config = Vcloud::VappConfig.new(vdc_config_dir)
       missing_vapps = Vcloud::MissingVappsForVdc.new(vdc_name, Vcloud::QueryRunner.new, vapp_config)
-      {
-        :missing_from_env => missing_vapps.from_environment,
-        :missing_from_files => missing_vapps.from_config
+      result = {
+          :missing_from_env => missing_vapps.from_environment,
+          :missing_from_files => missing_vapps.from_config
       }
+      cli_options[:yaml] ? print(result.to_yaml) : print(JSON.pretty_generate result)
     end
+    
   end
 end
